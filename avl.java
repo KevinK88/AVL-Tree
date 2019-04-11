516ac8cafb0112c39982cd96aa49697e72186ba2
KevinK88 <kevkwok0401@gmail.com>
2019-04-11 13:36:33 -0700

    This is another version.

--- avl.java
+++ avl.java
@@ -272,55 +272,7 @@ public class avl {
            tree.insert(n);
        }
 
-          // Testing case 1 -- Left-left
-       
-       //  tree.insert(13);
-       // tree.insert(15);
-       // tree.insert(10);
-       // tree.insert(8);
-       // tree.insert(12);
-       // tree.insert(7);
-       //  tree.insert(9);
-        // CASE 1 PASSED
 
-
-        // Testing case 2 -- Right - Right
-      //   tree.insert(30);
-      // tree.insert(5);
-      // tree.insert(35);
-      // tree.insert(32);
-      // tree.insert(40);
-      // tree.insert(45);
-      // Case 2 passed
-
-        //Testing case 3 Left - right
-      //    tree.insert(30);
-      // tree.insert(15);
-      // tree.insert(40);
-      // tree.insert(10);
-      // tree.insert(18);
-      // tree.insert(16);
-      // tree.insert(19);
-         // Case 3 passed
- 
-
-      // Testing Case 4 Right - Left
-      // tree.insert(5);
-   //       tree.insert(2);
-   //       tree.insert(7);
-   //       //tree.insert(38);
-   //       tree.insert(1);
-   //       tree.insert(4);
-   //       tree.insert(6);
-   //       tree.insert(9);
-   //       tree.insert(3);
-   //       tree.insert(16);
-   //       tree.insert(15);
-         // CASE 4 Passed
-        tree.print();
-        tree.print_max();
-       // tree.remove(4);
-    
       
      
     }
